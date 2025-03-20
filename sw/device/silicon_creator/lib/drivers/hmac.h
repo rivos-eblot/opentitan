d4d5d51b55447aa60341407dfc041f7678b5c3b5
Emmanuel Blot <eblot@rivosinc.com>
2025-03-20 19:44:54 +0100

    [silicon_creator,hmac] workaround double implementation for hmac functions
    This file declares an inline implementation for hmac_sha256_init and
    hmac_sha256_final. This file is then imported by the mock C+++
    implementation which re-implement these functions with a C++ implementation.
    
    Uses __cplusplus exclusion as a temporary workaround, although the real
    fix should better be to remove the inline implementation from the .h
    file.
    
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>
    (cherry picked from commit 3b5779d2d89358401cb33e7e470b14a5837b6d0e)

--- sw/device/silicon_creator/lib/drivers/hmac.h
+++ sw/device/silicon_creator/lib/drivers/hmac.h
@@ -66,10 +66,14 @@ void hmac_sha256_start(void);
 /**
  * Configures and starts HMAC in SHA256 mode with little-endian output.
  */
+#ifdef __cplusplus
+void hmac_sha256_init(void);
+#else
 inline void hmac_sha256_init(void) {
   hmac_sha256_configure(false);
   hmac_sha256_start();
 }
+#endif
 
 /**
  * Sends `len` bytes from `data` to the SHA2-256 function.
@@ -120,9 +124,13 @@ void hmac_sha256_final_truncated(uint32_t *digest, size_t len);
  *
  * @param[out] digest Buffer to copy digest to.
  */
+#ifdef __cplusplus
+void hmac_sha256_final(hmac_digest_t *digest);
+#else
 inline void hmac_sha256_final(hmac_digest_t *digest) {
   hmac_sha256_final_truncated(digest->digest, ARRAYSIZE(digest->digest));
 }
+#endif
 
 /**
  * Convenience single-shot function for computing the SHA-256 digest of a
